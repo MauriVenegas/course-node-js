b6e4230aad5ac36f64b4919d50a66d50e1c33654
Mauricio Venegas <mvenegas.developer@gmail.com>
2024-04-25 20:56:55 -0400

    Arquitectura MVC

---
+++ class-4/models/local-file-system/movie.js
@@ -0,0 +1,53 @@
+import { log } from "node:console";
+import { readJSON } from "../../utils.js";
+const movies = readJSON('./movies.json')
+import { randomUUID } from 'node:crypto'
+
+
+export class MovieModel {
+  static async getAll ({ genre }) {
+    if (genre) {
+      return movies.filter(
+        movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+      )
+    }
+    return movies
+  }
+
+  static async getById ({ id }){
+    const movie = movies.find(movie => movie.id === id)
+    return movie
+  }
+
+  static async create (input) {
+    const newMovie = {
+      id: randomUUID(),
+      ...input
+    }
+
+    movies.push(newMovie)
+    return newMovie
+  }
+
+  static async delete ({ id }) {
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) return false
+
+    movies.splice(movieIndex, 1)
+    return true
+  }
+
+  static async update ({id, input}) {
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) return false
+
+    // Actuliza los valores usando ... (spread) => copia y remplaza valores
+    const updateMovie = {
+      ...movies[movieIndex],
+      ...input
+    }
+    return movies[movieIndex] = updateMovie
+    }
+}
